3d83195ed6b9863042f9a6cb4c5e23fe14039f99
NSchmidt48067 <m265646@usna.edu>
2024-04-05 11:28:07 -0400

    rate

--- Rate.java
+++ Rate.java
@@ -12,7 +12,7 @@ import java.awt.event.*;
 public class Rate extends JPanel {
   private String val;
   private JLabel lab;
-  private JComboBox<double> cbox;
+  private JComboBox<Double> cbox;
 
   private class Selector implements ActionListener {
     public void actionPerformed(ActionEvent e) { update(); }
@@ -24,17 +24,17 @@ public class Rate extends JPanel {
     lab = new JLabel(val);
     lab.setPreferredSize(new Dimension(180,40));
 
-    double[] options ={3.50, 3.75, 4.00, 4.25, 4.50, 4.75, 5.00, 5.25, 5.50, 5.75, 6.00, 6.25, 6.50, 6.75, 7.00, 7.25, 7.50}; 
+    Double[] options ={3.50, 3.75, 4.00, 4.25, 4.50, 4.75, 5.00, 5.25, 5.50, 5.75, 6.00, 6.25, 6.50, 6.75, 7.00, 7.25, 7.50}; 
 
-    cbox = new JComboBox<double>( options );
+    cbox = new JComboBox<Double>( options );
     cbox.addActionListener(new Selector());
     add(lab);
     add(cbox);
   }
   
   public void update() {
-    int i = cbox.getSelectedIndex();
-    double way = cbox.getItemAt(i);
-    lab.setText(Rewriter.rewrite(way,val));
+    //int i = cbox.getSelectedIndex();
+    //double way = cbox.getItemAt(i);
+    //lab.setText(Rewriter.rewrite(way,val));
   }
 }
